e13322d2ceb5fa3300bcf5af1340e1ff36d21830
Sean Tolbert <sean@seans-mbp.mynetworksettings.com>
2023-10-26 09:51:15 -0400

    init

---
+++ app/(tabs)/three.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const three = () => {
+  return (
+    <View>
+      <Text>three</Text>
+    </View>
+  )
+}
+
+export default three
+
+const styles = StyleSheet.create({})
